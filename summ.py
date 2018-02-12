43834d7cead064adb215e098ec4dbd8ea9cfb0d8
VlArtem <rubfan197@gmail.com>
2018-02-12 10:56:05 +0800

    Сложнее

---
+++ summ.py
@@ -0,0 +1,9 @@
+def summator(a:int,b:int)->int:
+    return a+b
+
+if __name__ == "__main__":
+    result = summator(3,4)
+    print(f"Давайте сложим чего-нибудь 3 + 4 {result}")
+    input("\n Для продолженние нажмите эникей")
+    
+    
