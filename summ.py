8b49d8ca987a2fc564881c4d2206a27a7941d88e
SERUFIMR-PC\Admin <Serufim97@gmail.com>
2018-03-04 23:13:14 +0800

    Пофиксил багу issue #3

--- summ.py
+++ summ.py
@@ -5,7 +5,7 @@ def summator(a: int, b: int) -> int:
 def minusator(a: int, b: int) -> int:
     return a - b
 def multiply(a:int,b:int=2)->int:
-    return a*2
+    return a*b
 
 if __name__ == "__main__":
     result = summator(3, 4)
