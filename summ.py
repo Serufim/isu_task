2c80db8935f252c579a4982a80f86abcdec059d4
SERUFIMR-PC\Admin <Serufim97@gmail.com>
2018-03-04 21:23:46 +0800

    Добавил вычитание issue#1

--- summ.py
+++ summ.py
@@ -1,9 +1,12 @@
-def summator(a:int,b:int)->int:
-    return a+b
+def summator(a: int, b: int) -> int:
+    return a + b
+
+
+def minusator(a: int, b: int) -> int:
+    return a - b
+
 
 if __name__ == "__main__":
-    result = summator(3,4)
+    result = summator(3, 4)
     print(f"Давайте сложим чего-нибудь 3 + 4 {result}")
     input("\n Для продолженние нажмите эникей")
-    
-    
